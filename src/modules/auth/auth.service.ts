af06f915341a30b7f074e5ec943bc6fc18047bb7
dev_nayan <md.nayan8494@gmail.com>
2026-05-23 01:00:43 +0600

    User Login API created and JWT Token generated

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../../config";
 import { pool } from "../../db";
 import type { IUser } from "./auth.interface";
 
+//* CREATE USER
 const createUserIntoDB = async (payload: IUser) => {
   const { name, email, password, role } = payload;
 
@@ -21,6 +24,54 @@ const createUserIntoDB = async (payload: IUser) => {
   return result.rows[0];
 };
 
+//* LOGIN USER
+const loginUserIntoDB = async (payload: {
+  email: string;
+  password: string;
+}) => {
+  const { email, password } = payload;
+
+  const userData = await pool.query(
+    `
+    SELECT * FROM users
+    WHERE email = $1
+    `,
+    [email],
+  );
+
+  if (userData.rows.length === 0) {
+    throw new Error("Invalid Credentials!");
+  }
+
+  const user = userData.rows[0];
+
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
+    throw new Error("Invalid Credentials!");
+  }
+
+  delete user.password;
+
+  // JWT Token Generation
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_token_secret, {
+    expiresIn: "1d",
+  });
+
+  const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken, user };
+};
+
 export const authService = {
   createUserIntoDB,
+  loginUserIntoDB,
 };
