37964ce63664395bf1d139822ca0bff7b8e73dbc
dev_nayan <md.nayan8494@gmail.com>
2026-05-23 15:37:10 +0600

    Get All Issues API created

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -6,16 +6,28 @@ import { issueService } from "./issue.service";
 const createIssue = async (req: Request, res: Response) => {
   const user = req.user as JwtPayload;
   const newRequestBody = { ...req.body, id: user.id };
-  const result = await issueService.createIssueIntoDB(newRequestBody);
+  const userData = await issueService.createIssueIntoDB(newRequestBody);
 
   sendResponse(res, {
     statusCode: 201,
     success: true,
     message: "Issue created successfully",
-    data: result.rows[0],
+    data: userData.rows[0],
+  });
+};
+
+const getAllIssues = async (req: Request, res: Response) => {
+  const issues = await issueService.getAllIssuesFromDB();
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Issues retrieved successfully.",
+    data: issues,
   });
 };
 
 export const issueController = {
   createIssue,
+  getAllIssues,
 };
--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -16,6 +16,57 @@ const createIssueIntoDB = async (payload: IIssue & { id: number }) => {
   return result;
 };
 
+const getAllIssuesFromDB = async () => {
+  // Getting All Issues
+  const allIssues = await pool.query(
+    `
+      SELECT * FROM issues
+    `,
+  );
+
+  // Getting All the user's ids who created issues
+  const userIds = allIssues.rows.map((user) => user.id);
+
+  const dynamicPlaceholder = userIds
+    .map((id, index) => `$${index + 1}`)
+    .join(",");
+
+  // Getting users who created issues
+  const usersData = await pool.query(
+    `
+    SELECT id, name, role FROM users
+    WHERE id IN (${dynamicPlaceholder})
+    `,
+    userIds,
+  );
+
+  const users = usersData.rows;
+
+  const usersMap = users.reduce((acc, user) => {
+    acc[user.id] = user;
+    return acc;
+  }, {});
+
+  // Formatting All issues
+  const formattedIssues = allIssues.rows.map((issue) => ({
+    id: issue.id,
+    title: issue.title,
+    description: issue.description,
+    type: issue.type,
+    status: issue.status,
+    reporter: {
+      id: usersMap[issue.reporter_id].id,
+      name: usersMap[issue.reporter_id].name,
+      role: usersMap[issue.reporter_id].role,
+    },
+    created_at: issue.created_at,
+    updated_at: issue.updated_at,
+  }));
+
+  return formattedIssues;
+};
+
 export const issueService = {
   createIssueIntoDB,
+  getAllIssuesFromDB,
 };
